4f9ef1e6cdf1cd0229e884f50ec5c2944a3eef01
zo-el <joelulahanna@gmail.com>
2021-03-25 23:55:50 -0500

    update holo-nixpkg

--- config.nix
+++ config.nix
@@ -36,14 +36,14 @@ echo "<your publishing script here>"
   github = rec {
    # can be any github ref
    # branch, tag, commit, etc.
-   ref = "b6397dc43496ad95eab169bcdf0ab73731a7133d";
+   ref = "57f47c6bc38e16ad113f7d419221bea5a1ee9ae9";
 
    # the sha of what is downloaded from the above ref
    # note: even if you change the above ref it will not be redownloaded until
    #       the sha here changes (the sha is the cache key for downloads)
    # note: to get a new sha, get nix to try and download a bad sha
    #       it will complain and tell you the right sha
-   sha256 = "1hmafym5sk0kk3vhl5b05kmfd8jrpflp3qqc9r7lcd9l9xakkmfv";
+   sha256 = "1klqxh0glk57kqar9410knfij6r9vmk0hhb4ry40798dvs1cparw";
 
    # the github owner of the holonix repo
    owner = "Holo-Host";
